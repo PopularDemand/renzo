a2db1bd83ee7ec98a11f30f3d2cacb3f965be364
Alexa <alexaanderson090@gmail.com>
2017-11-11 21:10:15 -0600

    Add auth

--- src/components/screens/home/HomeScreen.js
+++ src/components/screens/home/HomeScreen.js
@@ -3,6 +3,59 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { Rotate } from '../../core/transforms';
 import { Earth } from '../../svg';
 
+async function signUp() {
+  try {
+    const body = JSON.stringify({
+      user: {
+        username: 'alexayay',
+        password: 'password',
+        password_confirmation: 'password',
+        email: 'wgpuck@yahoo.com'
+      }
+    });
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    const res = await fetch('https://zo-serve.herokuapp.com/users', {
+      method: 'POST',
+      headers,
+      body
+    });
+    const currentUser = await res.json();
+    console.log(currentUser);
+  } catch (error) {
+    console.error('error: ', error);
+  }
+}
+
+async function signOut() {
+  try {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    const res = await fetch('https://zo-serve.herokuapp.com/signout', {
+      method: 'DELETE',
+      headers
+    });
+    const currentUser = await res.json();
+    console.log(currentUser);
+  } catch (error) {
+    console.error('error: ', error);
+  }
+}
+
+async function getUsers() {
+  try {
+    const res = await fetch('https://zo-serve.herokuapp.com/users');
+    const users = await res.json();
+    console.log(users);
+  } catch (error) {
+    console.log('error: ', error);
+  }
+}
+
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
@@ -13,6 +66,14 @@ export default function HomeScreen({ navigation }) {
         <Button
           title="Play Now"
           onPress={() => navigation.navigate('Game')}
+        />
+        <Button
+          title="sign in"
+          onPress={signUp}
+        />
+        <Button
+          title="sign out"
+          onPress={signOut}
         />
       </View>
   );
